b3d5ea96812047b9d710d959aa777421be1ac74e
GitHub <noreply@github.com>
2023-02-12 12:21:34 +0000

    chore(deps): update deno dependencies

--- utils/transpile_test.ts
+++ utils/transpile_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.173.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts";
 import { MediaType, transpile } from "./transpile.ts";
 
 const codes = [
